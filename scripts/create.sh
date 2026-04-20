3e634dd04c45fed38a55637ec019f6c306252419
Vinícius Lino Figueirêdo <viniciusfigueiredo725@protonmail.com>
2026-04-20 16:04:39 -0300

    fix: allow multi word title

--- scripts/create.sh
+++ scripts/create.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 tmux setenv -u tmux_nb_create_title
-tmux command-prompt -p "Title:" "setenv tmux_nb_create_title %1"
-TITLE=$(tmux showenv tmux_nb_create_title | cut -d= -f2)
+tmux command-prompt -p "Title:" "setenv tmux_nb_create_title '%1'"
+TITLE=$(printf '%q' "$(tmux showenv tmux_nb_create_title | sed 's/^tmux_nb_create_title=\(.*\)/\1/')")
 
 if [ -n "$TITLE" ]; then
 	tmux display-popup -E -w 80% -h 80% "nb + --title $TITLE"
